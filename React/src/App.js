cd37166751f95adb5a0435804e525c8cb455031c
Yehudit kofman <y0556733201@gmail.com>
2025-04-20 02:42:52 +0300

    fix...

---
+++ React/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home';
+import CreateAdministrator from './Components/CreateAdministrator';
+
+import SighInAdministrator from './Components/SighInAdministrator';
+import Try from './Components/Try'; 
+
+
+function App() {
+  return (
+  
+     <>
+     <Try/>
+    </> 
+  );
+}
+
+export default App;
---
+++ React/src/Components/PersonalInfo.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const PersonalInfo = ({ formData, handleChange }) => (
+    <div className="personal-info">
+        <h2>Personal details</h2>
+        <div>
+            <label>Name:</label>
+            <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        </div>
+        <div>
+            <label>Email:</label>
+            <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </div>
+        <div>
+            <label>Phone:</label>
+            <input type="text" name="phone" value={formData.phone} onChange={handleChange} />
+        </div>
+    </div>
+);
+
+export default PersonalInfo;
