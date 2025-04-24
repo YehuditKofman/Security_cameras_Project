ba350ebeccca01c900b05a060261579ab9178b6d
Yehudit kofman <y0556733201@gmail.com>
2025-04-24 19:59:32 +0300

    nerge with Upluod

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,12 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
 import CreateAdministrator from './Components/CreateAdministrator';
-
 import SighInAdministrator from './Components/SighInAdministrator';
 import Try from './Components/Try'; 
 import CreatNewMember from './Components/AddMembers/CreatNewMember';
 import UploadVideo from './Components/UploadVideo/UploadVidea';
-
 import GetAllmembersName from './Components/GetAllMembersName/GetAllmembersName';
 
 
